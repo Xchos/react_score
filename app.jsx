e5db18f1e85db3d3c7742982e3c3dd65dd602679
Lukáš Zelenka <lukas.zelenka974@gmail.com>
2017-01-28 16:52:59 +0100

    initialScore feature added

--- app.jsx
+++ app.jsx
@@ -1,7 +1,7 @@
 var PLAYERS = [
-  {id: 1, name: "Lukáš Zelenka"},
-  {id: 2, name: "Jan Novák"},
-  {id: 3, name: "Jana Nováková"},
+  {id: 1, name: "Lukáš Zelenka", score: 30},
+  {id: 2, name: "Jan Novák", score: 35},
+  {id: 3, name: "Jana Nováková", score: 32},
 ];
 
 function Header(props) {
@@ -21,7 +21,7 @@ var Counter = React.createClass({
   propTypes: {},
   getInitialState: function() {
     return {
-      score: 0
+      score: this.props.initialScore,
     }
   },
   incrementScore: function(e) {
@@ -54,13 +54,14 @@ function Player(props) {
   return (
     <div className="player">
       <div className="player-name">{props.name}</div>
-      <Counter />
+      <Counter initialScore={props.score} />
     </div>
   );
 }
 
 Player.propTypes = {
   name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number.isRequired,
 };
 
 function Application(props) {
@@ -69,7 +70,7 @@ function Application(props) {
       <Header title={props.title} />
       <div className="players"> 
         {props.players.map(function(player){
-          return <Player name={player.name} key={player.id} />;
+          return <Player name={player.name} key={player.id} score={player.score} />;
         })}
       </div>
     </div>
@@ -80,7 +81,8 @@ Application.propTypes = {
   title: React.PropTypes.string,
   players: React.PropTypes.arrayOf(React.PropTypes.shape({
     name: React.PropTypes.string.isRequired,
-    id: React.PropTypes.number.isRequired    
+    id: React.PropTypes.number.isRequired,    
+    score: React.PropTypes.number.isRequired,    
   })).isRequired,
 };
 

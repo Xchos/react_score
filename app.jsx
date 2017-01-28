a3ae2714ad7d57df809f3562a881f22fff0c8476
Lukáš Zelenka <lukas.zelenka974@gmail.com>
2017-01-28 15:55:39 +0100

    header component

--- app.jsx
+++ app.jsx
@@ -1,25 +1,36 @@
+function Header(props) {
+  return(
+    <div className="header">
+        <h1>{props.title}</h1>
+      </div>
+  );
+}
+
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired
+};
+
 function Application(props) {
   return (
     <div className="scoreboard">
-      <div className="header">
-        <h1>{props.title}</h1>
-      </div>
-      
-      <div className="player">
-        <div className="player-name">Lukáš Zelenka</div>
-        <div className="counter">
-          <button className="counter-action decrement">-</button>
-          <div className="counter-score">30</div>
-          <button className="counter-action increment">+</button>
+      <Header title={props.title} />
+      <div className="players"> 
+        <div className="player">
+          <div className="player-name">Lukáš Zelenka</div>
+          <div className="counter">
+            <button className="counter-action decrement">-</button>
+            <div className="counter-score">30</div>
+            <button className="counter-action increment">+</button>
+          </div>
         </div>
-      </div>
 
-      <div className="player">
-        <div className="player-name">Jan Novák</div>
-        <div className="counter">
-          <button className="counter-action decrement">-</button>
-          <div className="counter-score">29</div>
-          <button className="counter-action increment">+</button>
+        <div className="player">
+          <div className="player-name">Jan Novák</div>
+          <div className="counter">
+            <button className="counter-action decrement">-</button>
+            <div className="counter-score">29</div>
+            <button className="counter-action increment">+</button>
+          </div>
         </div>
       </div>
     </div>
@@ -27,7 +38,11 @@ function Application(props) {
 }
 
 Application.propTypes = {
-  title: React.PropTypes.string.isRequired
+  title: React.PropTypes.string
 };
 
-ReactDOM.render(<Application title="Tabulka výsledků" />, document.getElementById('container'));
+Application.defaultProps = {
+  title: "Tabulka výsledků"
+};
+
+ReactDOM.render(<Application />, document.getElementById('container'));

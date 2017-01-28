519fea0d37fa42dcd3ef371686ac75f385e92be8
Lukáš Zelenka <lukas.zelenka974@gmail.com>
2017-01-28 15:32:39 +0100

    initial project

---
+++ app.jsx
@@ -0,0 +1,11 @@
+function Application() {
+  return (
+    <div className="scoreboard">
+      <div className="header">
+        <h1>Tabulka výsledků</h1>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById('container'));

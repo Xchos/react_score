486235d6ddc6639ce95868158523cea657a91817
Lukáš Zelenka <lukas.zelenka974@gmail.com>
2017-01-28 15:39:24 +0100

    static board

--- app.jsx
+++ app.jsx
@@ -4,6 +4,24 @@ function Application() {
       <div className="header">
         <h1>Tabulka výsledků</h1>
       </div>
+      
+      <div className="player">
+        <div className="player-name">Lukáš Zelenka</div>
+        <div className="counter">
+          <button className="counter-action decrement">-</button>
+          <div className="counter-score">30</div>
+          <button className="counter-action increment">+</button>
+        </div>
+      </div>
+
+      <div className="player">
+        <div className="player-name">Jan Novák</div>
+        <div className="counter">
+          <button className="counter-action decrement">-</button>
+          <div className="counter-score">29</div>
+          <button className="counter-action increment">+</button>
+        </div>
+      </div>
     </div>
   );
 }
